e8139c5adfc9e00d9465f52dcbfbf45af1e4275f
Nirutt Anstee <80709800+niruttanstee@users.noreply.github.com>
2023-06-04 22:36:44 +0100

    Add milestone command counter

--- cogs/hello.py
+++ cogs/hello.py
@@ -1,18 +1,21 @@
 import disnake
 from disnake.ext import commands
+import milestone
 
 
 class Hello(commands.Cog):
     """ Sends hello message """
-    
+
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.slash_command(description="Says Hello")
     async def hello(self, inter: disnake.ApplicationCommandInteraction):
+        await milestone.count_command()  # Counts the number command calls for milestone
         await inter.response.send_message("Hello <3")
 
     # Useful comments
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Hello(bot))
